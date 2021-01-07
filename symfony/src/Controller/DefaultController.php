eb37b59e9ffe38cdf9cda504988ea9117954c52f
Adrien <adrien.aguila-multner@etu.u-bordeaux.fr>
2021-01-07 20:21:17 +0100

    profile page

--- symfony/src/Controller/DefaultController.php
+++ symfony/src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,10 +22,26 @@ class DefaultController extends AbstractController
     /**
      * @Route("/about", name="about")
      */
-    public function aPropos(): Response
+    public function about(): Response
     {
-        return $this->render('modele.html.twig', [
+        return $this->render('default/modele.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile(): Response
+    {
+        if(($user = $this->getUser()) === NULL) {
+            return $this->redirectToRoute('app_login');
+        }
+        
+        $days = date_diff(new DateTime('now'), $user->getRegisterDate())->format('%d jours, %m mois et %y années');
+
+        return $this->render('default/profile.html.twig', [
+            'days' => $days,
         ]);
     }
 }
